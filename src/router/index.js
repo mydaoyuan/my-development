9a88ab354772f63adaa640b359de0880def5e547
tangdy <tdy853839625@qq.com>
2021-07-05 10:14:00 +0800

    fix:路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ChartView from '../components/chartView.vue'
 import PrintView from '../components/printView.vue'
+import TreeGraph from '../components/graph/treeGraph/graph.vue'
+import LineGraph from '../components/graph/lineGraph/index.vue'
+import MapGraph from '../components/graph/mapGraph/map.vue'
 
 Vue.use(VueRouter)
 
@@ -12,6 +15,21 @@ const routes = [
     component: ChartView
   },
   {
+    path: '/treeGraph',
+    name: 'TreeGraph',
+    component: TreeGraph
+  },
+  {
+    path: '/lineGraph',
+    name: 'LineGraph',
+    component: LineGraph
+  },
+  {
+    path: '/mapGraph',
+    name: 'MapGraph',
+    component: MapGraph
+  },
+  {
     path: '/printgraphics',
     name: 'printgraphics',
     component: PrintView
@@ -19,7 +37,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  base: '/temperature/',
+  base: '/',
   mode: 'history',
   routes
 })
