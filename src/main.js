00b66e04af03f7cb7e291a4766e7832c012e07f0
tangdy <tdy853839625@qq.com>
2022-02-14 14:20:54 +0800

    fix:增加ban工嗯呢

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { init } from 'console-ban'
 
 Vue.config.productionTip = false
 
+if (process.env.NODE_ENV !== 'development') {
+  init({
+    write: `<h1> QQ:${process.env.VUE_APP_QQ} 获取Vue源码 </h1>`
+  })
+}
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  mounted () {
+    document.dispatchEvent(new Event('render-event'))
+  }
 }).$mount('#app')
