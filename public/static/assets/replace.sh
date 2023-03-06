ad9b9cd6746f135058d698427d9fbb59857cc967
tangdy <tdy853839625@qq.com>
2023-03-06 20:09:54 +0800

    change shell

--- public/static/assets/replace.sh
+++ public/static/assets/replace.sh
@@ -5,7 +5,7 @@ js_file=$(ls *.js)
 css_file=$(ls *.css)
 
 # Replace script tag src attribute
-sed 's|<script src="/temperature/static/assets/index-[0-9a-f]\{8\}.js" type="module" crossorigin></script>|<script src="/temperature/static/assets/'"$js_file"'"></script>|g; s|<link href="/temperature/static/assets/index-[0-9a-f]\{8\}.css" rel="stylesheet" >|<link href="/temperature/static/assets/'"$css_file"'" rel="stylesheet" >|g' timeChat_source.html > timeChat.html
+sed 's|<script src="/temperature/static/assets/index-[0-9a-f]\{8\}.js" type="module" crossorigin></script>|<script src="/temperature/static/assets/'"$js_file"'" type="module" crossorigin></script>|g; s|<link href="/temperature/static/assets/index-[0-9a-f]\{8\}.css" rel="stylesheet" >|<link href="/temperature/static/assets/'"$css_file"'" rel="stylesheet" >|g' timeChat_source.html > timeChat.html
 
 
 
